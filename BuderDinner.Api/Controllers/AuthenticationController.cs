1519fbe9c2949d3612fa699b9e0a767f880eab9a
phancaovu <66910370+phancaovu@users.noreply.github.com>
2023-04-04 14:32:53 +0700

    Add project files.

---
+++ BuderDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuderDinner.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register( RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
